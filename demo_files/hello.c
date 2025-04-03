d925c9ba90130994929d260b75cb63164f3073c2
kazma <kazmatw@gmail.com>
2025-04-03 18:00:02 +0800

    Add hello.c

---
+++ demo_files/hello.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    MessageBoxA(NULL, "Hello, world!", "PE-bear Demo", MB_OK);
+    return 0;
+}
